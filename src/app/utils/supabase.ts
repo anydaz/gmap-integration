7e035f68df29ba6312b8e42d61da540d696510e3
andy darmawan <andy.daz.95@gmail.com>
2025-05-06 02:43:30 +0700

    use env var for supabase key

--- src/app/utils/supabase.ts
+++ src/app/utils/supabase.ts
@@ -1,8 +1,8 @@
 import { createClient } from "@supabase/supabase-js";
 
 export const supabase = createClient(
-  "https://zzzvljdbovpnyrocqhoh.supabase.co",
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inp6enZsamRib3Zwbnlyb2NxaG9oIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDY0NDcxMzMsImV4cCI6MjA2MjAyMzEzM30.OcMmRGBS9kzl9DULEuxrsDbgeGb72yCOgY8jD0bjtL4"
+  process.env.NEXT_PUBLIC_SUPABASE_URL!,
+  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
 );
 
 export const uploadToStorage = async (file: File) => {
